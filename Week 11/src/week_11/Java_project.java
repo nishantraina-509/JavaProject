602c0981e157e2b645b5b9350c0ae578a1ee64ba
Admin <Admin@DESKTOP-T9VHCN6>
2020-12-15 18:11:26 +0530

    my first program

---
+++ Week 11/src/week_11/Java_project.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package week_11;
+
+/**
+ * @author sandeepkaur-70
+ *
+ */
+public class Java_project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+System.out.println("sir sir");
+}
