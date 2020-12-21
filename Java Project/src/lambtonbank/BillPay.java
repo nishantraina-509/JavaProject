a5ee5a957b205537aecc65b3070b6f413bd90571
nishantraina-509 <75627297+nishantraina-509@users.noreply.github.com>
2020-12-21 08:16:55 -0500

    Error resolved

--- Java Project/src/lambtonbank/BillPay.java
+++ Java Project/src/lambtonbank/BillPay.java
@@ -1,40 +1,42 @@
-package LambtonBank;
+package lambtonbank;
+import java.io.IOException;
+
 /**
+ *
  * @author saneepkaur-070
  * @author nishantraina-509
- *
  */
-import java.io.IOException;
 import java.util.Scanner;
 
-import LambtonBank.Customerdetails;
+import lambtonbank.Customerdetails;
 
 public class BillPay {
-	public static void payments(int code, String usernumber) throws IOException{
-		String[] existing1 = db.readFile(usernumber);
-		Scanner keyboard = new Scanner(System.in);
+	public static void payments(int code, String custnumbr) throws IOException{
+		String[] old1 = db.readFile(custnumbr);
+		Scanner myinput = new Scanner(System.in);
 		if(code == 1) {
-			System.out.println("Please Enter Electricity Bill Amount");
-			int amount = keyboard.nextInt();
-			int amount2 = Integer.parseInt(existing1[8]) - amount;
-			existing1[8] = String.valueOf(amount2);
-			System.out.println("Electricity Bill Payment is Successful!");
+			System.out.println("|Enter the Bill Amount|");
+			int bill = myinput.nextInt();
+			int bill2 = Integer.parseInt(old1[8]) - bill;
+			old1[8] = String.valueOf(bill2);
+			System.out.println("|Bill Paid! Balance Left= "+old1[8]+"|");
 			
 		}
 		else if(code == 2) {
-			System.out.println("Paying Phone Bill of 50 CAD...");
-			int amount2 = Integer.parseInt(existing1[8]) - 50;
-			existing1[8] = String.valueOf(amount2);
-			System.out.println("Phone Bill is Successful!");
+			System.out.println("|Enter the Bill Amount|");
+			int bill = myinput.nextInt();
+			int bill2 = Integer.parseInt(old1[8]) - bill;
+			old1[8] = String.valueOf(bill2);
+			System.out.println("|Bill Paid! Balance Left= "+old1[8]+"|");
 		}
 		else if(code == 3) {
-			System.out.println("Please Enter Car Insurance Amount");
-			int amount = keyboard.nextInt();
-			int amount2 = Integer.parseInt(existing1[8]) - amount;
-			existing1[8] = String.valueOf(amount2);
-			System.out.println("Car Insurance Payment is Successful!");
+			System.out.println("|Enter the Bill Amount|");
+			int bill = myinput.nextInt();
+			int bill2 = Integer.parseInt(old1[8]) - bill;
+			old1[8] = String.valueOf(bill2);
+			System.out.println("|Bill Paid! Balance Left= "+old1[8]+"|");
 		}
-	db.clearFile(usernumber);
-	db.writeFile(usernumber,existing1);
+	db.clearFile(custnumbr);
+	db.writeFile(custnumbr,old1);
 	}
 }
