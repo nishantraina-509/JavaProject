4444e8c31abcf91b4b4ddfe349a2b16744ebaca9
nishantraina-509 <75627297+nishantraina-509@users.noreply.github.com>
2020-12-20 23:10:52 -0500

    Revert "Merge branch 'main' of https://github.com/nishantraina-509/JavaProject.git into main"
    This reverts commit 6c5eda4c501a0fa095ad884a6850ee0be9fc0fcc, reversing
    changes made to 6eecd5ad7be1124e9f02f6c8b9599c3cc27155c5.

--- Java Project/src/Banking/createaccount.java
+++
@@ -1,9 +0,0 @@
-package Banking;
-
-public class createaccount {
-
-	public static void main(String[] args) {
-		// TODOad
-	}
-
-}
--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # JavaProject
-
-Group Members
-
-Akshay Goswami 596
-
-Sandeep Kaur 070
-
-Manpreet Kaur 094
-
-Nishant Raina 509
